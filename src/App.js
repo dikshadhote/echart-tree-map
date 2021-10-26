737a45986fdeb0a15ff068938c09f1033780499a
dikshadhote <39937404+dikshadhote@users.noreply.github.com>
2021-10-26 16:31:13 +0530

    Feat :  basic Echart with ReactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+import React, { Component } from "react";
+import ReactEcharts from "echarts-for-react";
+class App extends Component {
+  getOption = () => {
+    return {
+      
+        xAxis: {
+          type: "category",
+          data: ["Mon", "Tue", "Wed", "Thurs", "Fri", "Sat", "Sun"],
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [
+          {
+            data: [820, 932, 901, 934, 1290, 1330, 1320],
+            type: "line",
+          },
+        ],
+      
+    };
+  };
+  render() {
+    return (
+      <ReactEcharts
+        option={this.getOption()}
+        style={{ height: "80vh", left: 50, top: 50, width: "90vw" }}
+      />
+    );
+  }
 }
-
 export default App;
